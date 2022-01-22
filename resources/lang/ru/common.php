fa7d3226297f08f37c86854b1fb9b3b88547c0e9
user <user>
2022-01-22 19:39:18 +0500

    feat(): Added translations

--- resources/lang/ru/common.php
+++ resources/lang/ru/common.php
@@ -5,6 +5,8 @@ return [
   'individual' => 'Физическое Лицо',
   'name' => 'Имя',
   'fullname' => 'ФИО',
+  'surname' => 'Фамилия',
+  'surname2' => 'Отчество',
   'birthday' => 'Дата рождения',
   'name.org' => 'Название организации',
   'password' => 'Пароль',
@@ -16,5 +18,15 @@ return [
   'reset_pass' => 'Сбросить пароль',
   'forgot_pass' => 'Забыли пароль?',
   'remember_me' => 'Запомнить меня',
-  'reset_pass' => 'Сбросить пароль'
+  'reset_pass' => 'Сбросить пароль',
+
+  //placeholders
+  'address_placeholder' => 'г. Ташкент ул. Истиклол д. 11',
+
+  //hints
+  'only_face_page_and_home' => '(Только лицевая сторона и прописка)',
+
+  //scans
+  'scan_passport' => 'Скан паспорта'
+
 ];
--- resources/lang/ru/org.php
+++ resources/lang/ru/org.php
@@ -4,9 +4,21 @@
 return [
   'fullname_director' => 'ФИО Директора',
   'tel_director' => 'Телефон Директора',
+
+  //docs
   'inn' => 'ИНН',
   'license' => 'Номер лицензии выданное хокимиатом',
   'agreement_contract' => 'Договор с авиакомпаниями или консолидаторами',
   'cadastre' => 'Договор, кадастр(если хозяин помещения)',
 
+  //scans
+  'scan_passport_file' => 'Скан паспорта директора',
+  'scan_inn' => 'Скан ИНН',
+  'scan_license' => 'Скан лицензии',
+  'scan_agreements_with_avia_and_cons' => 'Скан договора с авиакомпаниями или консолидаторами',
+  'scan_cadastre' => 'Скан кадастра',
+
+
+  //placeholders
+  'org_placeholder' => 'OOO Ваша организация'
 ];
