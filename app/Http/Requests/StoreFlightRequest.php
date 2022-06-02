8bcd3a9f1293b9eeccad12df094072d6cb6965aa
Igor Kuryakov <remrtehno@gmail.com>
2022-06-02 11:50:43 +0500

    fix(): Middleware sanitize fix

--- app/Http/Requests/StoreFlightRequest.php
+++ app/Http/Requests/StoreFlightRequest.php
@@ -26,9 +26,9 @@ class StoreFlightRequest extends FormRequest
         return [
             'flight' => 'required|min:2',
             'count_chairs' => 'required|min:1',
-            'price_adult' => 'required|min:1',
-            'price_child' => 'required|min:1',
-            'price_infant' => 'required|min:1',
+            'price_adult' => 'required|gte:0',
+            'price_child' => 'required|gte:0',
+            'price_infant' => 'required|gte:0',
             'date' => 'required|min:10',
             'date_arrival' => 'required|min:10',
             'logo' => 'image|mimes:jpg,jpeg,png,svg|max:2048',
