233c9653c23e75980f68fe7a7e2056d47b2c1484
Igor Kuryakov <remrtehno@gmail.com>
2022-06-11 18:48:28 +0500

    fix(): Can be deleted flight

--- resources/views/dashboard/flights/partials/action-buttons.blade.php
+++ resources/views/dashboard/flights/partials/action-buttons.blade.php
@@ -15,13 +15,18 @@
     <i class="fa fa-lg fa-fw fa-save"></i>
   </a>
 
+  @php
+    $isAsignedFlight = $assignedFlihts ?? false;
+    $canBeDeleted = !$isAsignedFlight && $row?->canBeDeleted();
+  @endphp
+
   @if (!Route::is('dashboard.top-flights.index'))
     <form method="POST"
-      @if ($row?->canBeDeleted()) action="{{ route('dashboard.flights.destroy', ['flight' => $id]) }}" @endif
+      @if ($canBeDeleted) action="{{ route('dashboard.flights.destroy', ['flight' => $id]) }}" @endif
       class="d-inline-block">
       @csrf
       @method('delete')
-      <button @if (!$row?->canBeDeleted()) disabled @endif onclick="return confirm('Уверены что хотите удалить?')"
+      <button @if (!$canBeDeleted) disabled @endif onclick="return confirm('Уверены что хотите удалить?')"
         class="btn btn-xs btn-default text-danger mx-1 shadow" title="Delete">
         <i class="fa fa-lg fa-fw fa-trash"></i>
       </button>
